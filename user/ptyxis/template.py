80f97cae9f5ac4d3b789dc20b6844dd8a3b6ae8d
shadow <shadowngoz@gmail.com>
2025-11-15 16:47:56 -0600

    Bump ptyxis to version 49.1

--- user/ptyxis/template.py
+++ user/ptyxis/template.py
@@ -1,26 +1,26 @@
 pkgname = "ptyxis"
-pkgver = "48.5"
+pkgver = "49.1"
 pkgrel = 0
 build_style = "meson"
 hostmakedepends = [
     "desktop-file-utils",
+    "doxygen",
     "gettext",
     "glib-devel",
     "gobject-introspection",
     "gtk+3-update-icon-cache",
     "meson",
-    "doxygen",
     "pkgconf",
 ]
 makedepends = [
     "gsettings-desktop-schemas-devel",
     "gtk4-devel",
+    "json-glib-devel",
     "libadwaita-devel",
     "libgtop-devel",
+    "libportal-devel",
     "pcre2-devel",
     "vte-gtk4-devel",
-    "json-glib-devel",
-    "libportal-devel",
 ]
 pkgdesc = "Ptyxis"
 license = "GPL-3.0-or-later"
@@ -28,4 +28,4 @@ url = "https://gitlab.gnome.org/chergert/ptyxis"
 source = (
     f"$(GNOME_SITE)/ptyxis/{pkgver[:-2]}/ptyxis-{pkgver}.tar.xz"
 )
-sha256 = "f91f8ebe1bb1aef6bf772a994d6b4f4d0832c28baf6384c27461bbc6af2aad8e"
+sha256 = "549b4bfb7e4cbce70a9969949cc6a7b843369e266cf02739666114dc540afcca"
